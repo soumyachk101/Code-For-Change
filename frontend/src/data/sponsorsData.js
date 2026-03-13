a2ebdc1ef170a1d7bab6b927f33c71640867eff2
bhumi110 <bhumisaraogi@gmail.com>
2026-03-13 21:33:26 +0530

    sponsor updated

--- frontend/src/data/sponsorsData.js
+++ frontend/src/data/sponsorsData.js
@@ -1,18 +1,18 @@
-import ComingSoon from "../assets/st-Coming-Soon.png";
+// import ComingSoon from "../assets/st-Coming-Soon.png";
 import Devfolio from "../assets/devfolio_text.png";
-
+import vercel from "../assets/vercel.png";
+import lfx from "../assets/LFX_Education.png";
+import cnd from "../assets/Cn dgp.png"
 const sponsorsData = {
     platinum: [
-        { name: 'Coming Soon', logo: ComingSoon },
+        { name: 'Cloud Native Durgapur', logo: cnd },
     ],
     gold: [
         { name: 'Devfolio', logo: Devfolio, link: "https://devfolio.co", alt: "DEVFOLIO LOGO" },
-        { name: 'Coming Soon', logo: ComingSoon },
     ],
     silver: [
-        { name: 'Coming Soon', logo: ComingSoon },
-        { name: 'Coming Soon', logo: ComingSoon },
-        { name: 'Coming Soon', logo: ComingSoon },
+        { name: 'Vercel', logo: vercel },
+        { name: 'The Linux Foundation', logo: lfx },
     ],
 };
 
